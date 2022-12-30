3f698fefb150526988f7af3d19718173a7fdb90e
jsb-dev <jsb-dev@outlook.com>
2022-12-30 11:27:09 +1100

    Implemented minLength password check in signup controller

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -3,25 +3,24 @@ import bcrypt from 'bcryptjs';
 import validator from 'validator';
 
 const signupController = async (req, res) => {
-  console.log('Controller received request with body:', req.body);
   // Get the email and password from the request body
   const { email, password } = req.body;
-  console.log(
-    'Email and password destructured, req.body prints as: ',
-    req.body
-  );
   // Validate the email address
-  console.log('Begin - validating email address');
   if (!validator.isEmail(email)) {
-    console.log('This process will now throw an error if invalid');
     return res.status(400).send({ error: 'Invalid email address' });
   }
 
+  // Check the length of the password
+  if (password.length < 7) {
+    console.error('Password must be at least 7 characters long');
+    return res
+      .status(400)
+      .send({ error: 'Password must be at least 7 characters long' });
+  }
+
   // Hash the password
-  console.log('Begin - hashing password');
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  console.log('Begin - creating new user document');
   try {
     // Create a new user with the hashed password
     const user = new User({ email, password: hashedPassword });
@@ -41,9 +40,6 @@ const signupController = async (req, res) => {
     console.log(user);
   } catch (error) {
     // If there was an error saving the user, send a 400 error
-    console.log(
-      'Error saving user to database - Error is somewhere between this console log and the last one'
-    );
     console.log(error);
     res.status(400).send(error);
   }
